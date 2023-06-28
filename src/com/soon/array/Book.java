cedb508cfb2c1a3869fe75fb837c1d23a6c52772
soonable@naver.com <soonable@naver.com>
2023-06-28 13:10:07 +0900

    feat : 객체 배열 깊은 복사

--- src/com/soon/array/Book.java
+++ src/com/soon/array/Book.java
@@ -22,4 +22,8 @@ public class Book {
         return this.title;
 
     }
+
+    public String getAuthor() {
+        return this.author;
+    }
 }
--- test/com/soon/array/ArrayCopyTest.java
+++ test/com/soon/array/ArrayCopyTest.java
@@ -3,6 +3,7 @@ package com.soon.array;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,5 +26,23 @@ public class ArrayCopyTest {
         // 객체 주소 값을 복사함으로 원본 객체 배열 수정시 값은 값이 바뀜
         books[3].setTitle("SLAMDUNK");
         assertThat(copyBooks[3].getTitle()).isEqualTo("SLAMDUNK");
+    }
+
+    @Test
+    void deepCopy() {
+        Book[] books = new Book[5];
+        IntStream.range(0, books.length)
+                .forEach(i -> books[i] = new Book("귀멸의 칼날" + (i + 1), "SOON"));
+
+        Book[] copyBooks = new Book[5];
+        IntStream.range(0, books.length)
+                .forEach(i -> copyBooks[i] = new Book(books[i].getTitle(), books[i].getAuthor()));
+
+        // 값만 복사하고 객체 주소값은 다름
+        assertThat(copyBooks).isNotEqualTo(books);
+
+        // 객체 주소값이 다르므로 원본 배열의 수정에 영향을 받지 않는다. 깊은 복사
+        books[3].setTitle("SLAMDUNK");
+        assertThat(copyBooks[3].getTitle()).isNotEqualTo("SLAMDUNK");
     }
 }
