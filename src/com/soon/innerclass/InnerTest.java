e89ccf60c6694ad06174c2a5f41593f9ea239be0
soonable@naver.com <soonable@naver.com>
2023-07-05 11:02:40 +0900

    feat : static inner class

--- src/com/soon/innerclass/InnerTest.java
+++ src/com/soon/innerclass/InnerTest.java
@@ -29,6 +29,22 @@ class OutClass {
 //        }
     }
 
+    static class InStaticClass {
+        int inNum = 100;
+        static int sInNum = 200;
+
+        void inTest() {
+            System.out.println("inNum = " + inNum + " using inner class variable");
+            System.out.println("sInNum = " + sInNum + " using inner class static variable");
+            System.out.println("sNum = " + sNum + " using outer class static variable");
+        }
+
+        static void sTest() {
+            System.out.println("sNum = " + sNum + " using outer class static variable");
+            System.out.println("sInNum = " + sInNum + " using inner class static variable");
+        }
+    }
+
     public void usingInClass() {
         inClass.inTest();
     }
@@ -39,5 +55,11 @@ public class InnerTest {
     public static void main(String[] args) {
         OutClass outer = new OutClass();
         outer.usingInClass();
+
+        System.out.println("================");
+
+        OutClass.InStaticClass.sTest();
+        OutClass.InStaticClass inStaticClass = new OutClass.InStaticClass();
+        inStaticClass.inTest();
     }
 }
