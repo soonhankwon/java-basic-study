b243d217c71677d8b7974d88be283caf3a0b3fef
soonable@naver.com <soonable@naver.com>
2023-07-05 12:10:22 +0900

    feat : functional interface, lambda

--- src/com/soon/lambda/AddTest.java
+++ src/com/soon/lambda/AddTest.java
@@ -5,5 +5,8 @@ public class AddTest {
     public static void main(String[] args) {
         Add addF = (x, y) ->  x + y;
         System.out.println(addF.add(3, 5));
+
+        MyMaxNumber max = (x, y) -> x >= y ? x : y;
+        System.out.println(max.getMaxNumber(10, 9));
     }
 }
---
+++ src/com/soon/lambda/MyMaxNumber.java
@@ -0,0 +1,6 @@
+package com.soon.lambda;
+
+@FunctionalInterface
+public interface MyMaxNumber {
+    int getMaxNumber(int num1, int num2);
+}
