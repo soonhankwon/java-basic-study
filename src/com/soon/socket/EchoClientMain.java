65fbc3fcb678e8f2f43a7e19340e00e5a526a85d
soonable@naver.com <soonable@naver.com>
2023-07-06 16:15:54 +0900

    feat : socket, echo server and client

---
+++ src/com/soon/socket/EchoClientMain.java
@@ -0,0 +1,34 @@
+package com.soon.socket;
+
+import java.io.*;
+import java.net.Socket;
+
+public class EchoClientMain {
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 50000);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            bw.write("Hello, JAVA + \n");
+            bw.flush();
+
+            System.out.println(br.readLine());
+            System.out.println(br.readLine());
+            System.out.println(br.readLine());
+
+            bw.close();
+            br.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+}
